de8b554ae8b3c9283014290c2238a7f666726956
e175089p <oscar.gloaguen@etu.univ-nantes.fr>
2019-03-26 12:17:47 +0100

    zgzrg

--- player.js
+++ player.js
@@ -12,9 +12,44 @@ $('#player').hover(function(){
     $('.info').toggleClass('up');
 });
 
+function getMinSec(sec) {
+    let secs = Math.floor(sec % 60);
+
+    return Math.floor(sec / 60) + ":" + (secs < 10 ? "0" : "") + secs;
+}
+
 //music player settings
 
-let audio = new Audio('http://music.dawnfoxes.com/_fxs_/_upls_/_sngs_/UK/clean_bandit-symphony-ft-zara_larsson.mp3');
+class Playlist {
+    constructor(...paths) {
+        this.songs = paths.map(path => new Audio(path));
+        this.songs.forEach(song => {
+            // TODO
+        });
+        this.index = 0;
+    }
+
+    get current() {
+        return this.songs[this.index];
+    }
+
+    play() {
+        this.current.play();
+    }
+
+    pause() {
+        this.current.pause();
+    }
+}
+
+let list = new Playlist('pizza.mp3');
+
+setInterval(() => {
+    let song = list.current;
+    $(".time--current").text(getMinSec(song.currentTime));
+    $(".time--total").text("-" + getMinSec(song.duration - song.currentTime));
+    $(".fill").css("width", Math.round(100 * song.currentTime / song.duration) + "%");
+}, 100);
 
 $('.pause').hide(); //hide pause button until clicked
 
